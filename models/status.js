64deb5c08e18b791d55efddfde4bdd84c7fe0c1a
Jay Gupta <jayguptazzz@gmail.com>
2019-11-23 19:12:05 +0530

    availability status

---
+++ models/status.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+const dbConfig = require("../dbconfig");
+
+var status = new mongoose.Schema({
+    status_code: { type: Number },
+    status_datetime: { type: Date, default: new Date() },
+    email: { type: String },
+    status_message: { type: String },
+    email: { type: String },
+    is_Active: { type: Boolean, default: true },
+    availability: {
+        fromDate: { type: Date },
+        toDate: { type: Date }
+    }
+});
+
+
+module.exports.getModel = function () {
+    let connection = dbConfig.connect();
+    return connection.model("status", status);
+};
+
+
+
---
+++ routes/user/status.js
@@ -0,0 +1,67 @@
+
+
+exports.status = function (app, controller, error, auth, middleware) {
+
+    app.route("/user/get-status").get(auth, function (request, response) {
+        try {
+            controller.getStatus(request, response);
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
+
+    app.route("/user/get-active-status").get(auth, function (request, response) {
+        try {
+            controller.getActiveStatus(request, response);
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
+
+    app.route("/user/add-status").post(auth, function (request, response) {
+        try {
+            controller.addStatus(request, response);
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
+
+    app.route("/user/update-status").put(function (request, response) {
+        try {
+            controller.updateStatus(request, response);
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
+
+    app.route("/user/delete-status/:id").delete(function (request, response) {
+        try {
+            controller.deleteStatus(request, response);
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
+
+    app.route("/user/hide-status").patch(function (request, response) {
+        try {
+            controller.hideStatus(request, response);
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
+
+    app.route("/user/add-availability").post(auth, function (request, response) {
+        try {
+            controller.addAvailability(request, response);
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
+}
