ccb3f5efb224fa6d4cd281d020d1f01ae4ee3063
Jay Gupta <jayguptazzz@gmail.com>
2020-03-22 02:53:40 +0530

    passportjs google login

--- controllers/user/socialAuth.js
+++ controllers/user/socialAuth.js
@@ -5,13 +5,26 @@ module.exports.socialAuth = function (utils, collection) {
 
     return {
 
-        checkUserExistAndSignUp: (socialProfileDetails) => {
+        checkUserExistAndSignUp: (socialProfileDetails, socialAccountType) => {
             return new Promise((resolve, reject) => {
-                let accountDetails = {
-                    profile_id: socialProfileDetails.id,
-                    email: socialProfileDetails.email,
-                    name: (socialProfileDetails && socialProfileDetails.first_name) ? (socialProfileDetails.last_name ? `${socialProfileDetails.first_name} ${socialProfileDetails.last_name}` : socialProfileDetails.first_name) : ""
+                let accountDetails = {};
+                if (socialAccountType === 'facebook') {
+                    accountDetails = {
+                        profile_id: socialProfileDetails.id,
+                        email: socialProfileDetails.email,
+                        name: (socialProfileDetails && socialProfileDetails.first_name) ? (socialProfileDetails.last_name ? `${socialProfileDetails.first_name} ${socialProfileDetails.last_name}` : socialProfileDetails.first_name) : ""
+                    }
+                } else {
+                    accountDetails = {
+                        profile_id: socialProfileDetails.sub,
+                        email: socialProfileDetails.email,
+                        name: socialProfileDetails.name,
+                        profilePic: socialProfileDetails.picture,
+                        is_email_verified: socialProfileDetails.email_verified,
+                    }
+
                 }
+
 
                 User.findOne({ "email": accountDetails.email }).then((userDetails) => {
                     if (!userDetails) {
@@ -23,7 +36,7 @@ module.exports.socialAuth = function (utils, collection) {
                             reject(error);
                         })
 
-                    } else {
+                    } else if (!userDetails.profile_id) {
                         User.findOneAndUpdate({ "email": accountDetails.email }, { $set: { "is_active": true, profile_id: socialProfileDetails.id } }).then((result) => {
                             if (result) {
                                 resolve(result);
@@ -31,6 +44,8 @@ module.exports.socialAuth = function (utils, collection) {
                         }).catch((error) => {
                             reject(error);
                         })
+                    } else {
+                        resolve(accountDetails);
                     }
 
                 })
--- routes/user/socialAuth.js
+++ routes/user/socialAuth.js
@@ -2,10 +2,13 @@
 
 exports.socialAuth = function (app, controller, error, auth, middleware) {
 
+    // Required dependencies 
+
     const passport = require('passport')
         , FacebookStrategy = require('passport-facebook').Strategy
         , cookieParser = require('cookie-parser')
         , session = require('express-session')
+        , GoogleStrategy = require('passport-google-oauth20')
 
 
     passport.serializeUser(function (user, done) {
@@ -17,7 +20,7 @@ exports.socialAuth = function (app, controller, error, auth, middleware) {
     });
 
 
-    // Use the FacebookStrategy within Passport.
+    // Use the Facebook Strategy within Passport.
 
     passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_API_KEY,
@@ -30,7 +33,7 @@ exports.socialAuth = function (app, controller, error, auth, middleware) {
             process.nextTick(async function () {
                 try {
                     //Check whether the User exists or not using profile.id
-                    profile = await controller.checkUserExistAndSignUp(profile._json, callback);
+                    profile = await controller.checkUserExistAndSignUp(profile._json, "facebook");
                     return callback(null, profile[0]);
                 } catch (error) {
                     return callback(true, error);
@@ -83,4 +86,73 @@ exports.socialAuth = function (app, controller, error, auth, middleware) {
         res.redirect('/login')
     }
 
+
+
+    // Google Strategy config
+
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_API_KEY,
+        clientSecret: process.env.GOOGLE_API_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+        (accessToken, refreshToken, profile, callback) => {
+            process.nextTick(async function () {
+                try {
+                    //Check whether the User exists or not using email
+                    await controller.checkUserExistAndSignUp(profile._json, "google");
+                    return callback(null, profile);
+                } catch (error) {
+                    return callback(true, error);
+                }
+
+
+            });
+        }
+    ));
+
+    // Middleware to check if the user is authenticated
+    function isUserAuthenticated(req, res, next) {
+        if (req.user) {
+            next();
+        } else {
+            res.send('You must login!');
+        }
+    }
+
+    // Routes
+    app.get('/', (req, res) => {
+        res.render('template', { user: req.user });
+    });
+
+    // passport.authenticate middleware is used here to authenticate the request
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email'], // Used to specify the required data
+    }));
+
+    // The middleware receives the data from Google and runs the function on Strategy config
+    app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+       
+        res.redirect('/user/google-login');
+
+    });
+
+    // Secret route
+    app.get('/secret', isUserAuthenticated, (req, res) => {
+        res.send('You have reached the secret route');
+    });
+
+    // Logout route
+    app.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.route("/user/google-login").get(function (request, response) {
+        try {
+            response.render('template', { user: { name: request.user.displayName } });
+        }
+        catch (err) {
+            error(err, response)
+        }
+    })
 }
