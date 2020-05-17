3a6791ce0b569733fd5a804b80febe11417879bc
Jay Gupta <jayguptazzz@gmail.com>
2020-05-18 01:22:35 +0530

    social login with live domain

--- controllers/user/socialAuth.js
+++ controllers/user/socialAuth.js
@@ -1,3 +1,7 @@
+var jwt = require('jsonwebtoken');
+const auth = require("../../lib/auth");
+const responseFile = require('../../lib/response');
+
 
 
 module.exports.socialAuth = function (utils, collection) {
@@ -52,6 +56,55 @@ module.exports.socialAuth = function (utils, collection) {
             })
 
         },
+
+        generateSocialToken: (socialProfileDetails) => {
+            return new Promise((resolve, reject) => {
+                jwt.sign(socialProfileDetails, process.env.SOCIAL_SECRET_KEY, { expiresIn: parseInt(process.env.SOCIAL_SECRET_KEY_VALID_TIME) }, function (err, token) {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(token);
+                });
+            })
+        },
+
+        authenticateSocialUser: (request, response) => {
+
+            let token = request.body.socialToken;
+            jwt.verify(token, process.env.SOCIAL_SECRET_KEY, function (err, data) {
+                if (err && err.name === 'TokenExpiredError')
+                    return response.status(200).send({ error: true, code: 'TokenExpiredError', message: 'The token has been expired.' })
+
+                if (err && err.name != 'TokenExpiredError')
+                    return response.status(200).send({ error: true, message: 'Unauthorized Access.' })
+                // request.headers.payload = data;
+
+                if (data && data.email) {
+                    User.findOne({ email: data.email }).then(async (userDetails) => {
+                        let payload = {
+                            id: userDetails._id
+                            // email: userDetails.email,
+                            // name: userDetails.name,
+                            // mobile: userDetails.mobile
+                        }
+                        let token = await auth.generateAuthToken(payload);
+                        let output = {
+                            error: false,
+                            msg: responseFile[4001]['msg'],
+                            code: responseFile[4001]['code'],
+                            token: token,
+                            data: payload
+                        }
+                        response.status(200).send(output);
+                    }).catch((error) => {
+                        utils.sendResponse(response, true, 500, 1000);
+                    })
+                } else {
+                    return response.status(401).send({ error: true, message: 'Unauthorized Access.' })
+                }
+            })
+
+        }
 
 
     }
--- schemas/models/auth.js
+++ schemas/models/auth.js
@@ -4,32 +4,38 @@ module.exports = {
         "properties": {
             "name": { "type": ["string"] },
             "email": { "type": ["string"] },
-            "password": { "type": ["string"], "minLength": 8  },
+            "password": { "type": ["string"], "minLength": 8 },
             "mobile": { "type": ["string"], "maxLength": 12, "minLength": 12 }
         },
-        "required": [ "mobile", "password" ]
+        "required": ["mobile", "password"]
     },
     logIn: {
         "properties": {
             "mobile": { "type": ["string"] },
             "password": { "type": ["string"] },
         },
-        "required": [ "mobile", "password" ]       
+        "required": ["mobile", "password"]
     },
     verifyEmailOtp: {
         "properties": {
             "email": { "type": ["string"] },
             "code": { "type": ["string"] }
         },
-        "required": [ "email", "code" ] 
+        "required": ["email", "code"]
     },
     verifyMobileOtp: {
         "properties": {
             "mobile": { "type": ["number"] },
             "code": { "type": ["number"] }
-        }
+        },
+        "required": ["mobile", "code"]
     },
-    "required": [ "mobile", "code" ] 
+    register: {
+        "properties": {
+            "mobile": { "type": ["string"], "maxLength": 12, "minLength": 12 }
+        },
+        "required": ["mobile"]
+    },
 }
 
 
