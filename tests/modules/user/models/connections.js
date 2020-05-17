32dad34ae2b5e4dc2159571695f875b625f86c21
amitsarraf <amitsarraf80@gmail.com>
2020-05-17 03:55:09 -0700

    connection

--- tests/modules/user/models/connections.js
+++ tests/modules/user/models/connections.js
@@ -1,11 +1,52 @@
 module.exports = function (appUrl, chai, should, assert, models) {
     return [
         {
-            description: "Get-Connections Case-1  Connection-User-Controller",
+            description: "Add-Connections Case-1  Connections-User-Controller",
             callback: function (done) {
 
                 chai.request(appUrl)
-                    .get("/user/get-connections")
+                    .post("user/add-connection")
+                    .send(models.connections.appConnections1.data)
+                    .end(function (err, res) {
+                        try {
+                            res.should.have.status(200);
+                            res.body.should.have.property('error', false);
+                            res.body.should.have.property('token');
+                            res.body.should.have.property('msg');
+                            res.body.should.have.property('code', 4027);
+                            done();
+                        }
+                         catch (error) {
+                            done(error);
+                        }
+                    });
+            }
+        },
+        {
+            description: "Add-Connections Case-2  Connections-User-Controller",
+            callback: function (done) {
+
+                chai.request(appUrl)
+                    .post("user/add-connection")
+                    .end(function (err, res) {
+                        try {
+                            res.should.have.status(401);
+                            res.body.should.have.property('error', true);
+                            res.body.should.have.property('msg');
+                            done();
+                        }
+                         catch (error) {
+                            done(error);
+                        }
+                    });
+            }
+        },
+        {
+            description: "Get-Connections Case-1  Connections-User-Controller",
+            callback: function (done) {
+
+                chai.request(appUrl)
+                    .get("user/get-connections")
                     .set('Authorization', process.env.token)
                     .end(function (err, res) {
                         try {
@@ -14,6 +55,7 @@ module.exports = function (appUrl, chai, should, assert, models) {
                             res.body.should.have.property('msg');
                             res.body.should.have.property('code', 4028);
                             res.body.should.have.property('data');
+                            done();
                         }
                          catch (error) {
                             done(error);
@@ -22,16 +64,17 @@ module.exports = function (appUrl, chai, should, assert, models) {
             }
         },
         {
-            description: "Get-Connections Case-2  Connection-User-Controller",
+            description: "Get-Connections Case-2  Connections-User-Controller",
             callback: function (done) {
 
                 chai.request(appUrl)
-                    .get("/user/get-connections")
+                    .get("user/get-connections")
                     .end(function (err, res) {
                         try {
                             res.should.have.status(401);
                             res.body.should.have.property('error', true);
                             res.body.should.have.property('msg');
+                            done();
                         }
                          catch (error) {
                             done(error);
@@ -40,11 +83,11 @@ module.exports = function (appUrl, chai, should, assert, models) {
             }
         },
         {
-            description: "Get-Active-Connection Case-1  Connection-User-Controller",
+            description: "Get-Active-Connection Case-1  Connections-User-Controller",
             callback: function (done) {
 
                 chai.request(appUrl)
-                    .get("/user/get-active-connections")
+                    .get("user/get-active-connections")
                     .set('Authorization', process.env.token)
                     .end(function (err, res) {
                         try {
@@ -52,7 +95,7 @@ module.exports = function (appUrl, chai, should, assert, models) {
                             res.body.should.have.property('error', false);
                             res.body.should.have.property('msg');
                             res.body.should.have.property('code', 4028);
-                        
+                            done();
                         }
                          catch (error) {
                             done(error);
@@ -61,18 +104,17 @@ module.exports = function (appUrl, chai, should, assert, models) {
             }
         },
         {
-            description: "Get-Active-Connections Case-2  Connection-User-Controller",
+            description: "Get-Active-Connections Case-2  Connections-User-Controller",
             callback: function (done) {
 
                 chai.request(appUrl)
-                    .get("/user/get-active-connections")
+                    .get("user/get-active-connections")
                     .end(function (err, res) {
                         try {
                             res.should.have.status(401);
                             res.body.should.have.property('error', true);
                             res.body.should.have.property('msg');
-                            
-                        
+                            done();
                         }
                          catch (error) {
                             done(error);
@@ -81,88 +123,11 @@ module.exports = function (appUrl, chai, should, assert, models) {
             }
         },
         {
-            description: "Add-Connections Case-1  Connection-User-Controller",
+            description: "Delete-Connections Case-1  Connections-User-Controller",
             callback: function (done) {
 
                 chai.request(appUrl)
-                    .post("/user/add-connection")
-                    .send(models.connections.appConnections1.data)
-                    .end(function (err, res) {
-                        try {
-                            res.should.have.status(200);
-                            res.body.should.have.property('error', false);
-                            res.body.should.have.property('msg');
-                            res.body.should.have.property('code', 4027);
-                        }
-                         catch (error) {
-                            done(error);
-                        }
-                    });
-            }
-        },
-        {
-            description: "Add-Connections Case-2  Connection-User-Controller",
-            callback: function (done) {
-
-                chai.request(appUrl)
-                    .post("/user/add-connection")
-                    .send(models.connections.appConnections2.data)
-                    .end(function (err, res) {
-                        try {
-                            res.should.have.status(400);
-                            res.body.should.have.property('error', true);
-                            res.body.should.have.property('msg');
-                        }
-                         catch (error) {
-                            done(error);
-                        }
-                    });
-            }
-        },
-        {
-            description: "Add-Connections Case-3  Connection-User-Controller",
-            callback: function (done) {
-
-                chai.request(appUrl)
-                    .post("/user/add-connection")
-                    .send(models.connections.appConnections3.data)
-                    .end(function (err, res) {
-                        try {
-                            res.should.have.status(400);
-                            res.body.should.have.property('error', true);
-                            res.body.should.have.property('msg');
-                        }
-                         catch (error) {
-                            done(error);
-                        }
-                    });
-            }
-        },
-        {
-            description: "Add-Connections Case-4  Connection-User-Controller",
-            callback: function (done) {
-
-                chai.request(appUrl)
-                    .post("/user/add-connection")
-                    .send(models.connections.appConnections4.data)
-                    .end(function (err, res) {
-                        try {
-                            res.should.have.status(400);
-                            res.body.should.have.property('error', true);
-                            res.body.should.have.property('msg');
-                        }
-                         catch (error) {
-                            done(error);
-                        }
-                    });
-            }
-        },
-        {
-            description: "Delete-Connections Case-1  Connection-User-Controller",
-            callback: function (done) {
-
-                chai.request(appUrl)
-                    .delete("/user/delete-connection/:id")
+                    .delete("user/delete-connection/:id")
                     .set('Authorization', process.env.token)
                     .end(function (err, res) {
                         try {
@@ -170,6 +135,7 @@ module.exports = function (appUrl, chai, should, assert, models) {
                             res.body.should.have.property('error', false);
                             res.body.should.have.property('msg');
                             res.body.should.have.property('code', 4029);
+                            done();
                         }
                          catch (error) {
                             done(error);
@@ -179,16 +145,17 @@ module.exports = function (appUrl, chai, should, assert, models) {
             }
         },
         {
-            description: "Delete-Connections Case-2  Connection-User-Controller",
+            description: "Delete-Connections Case-2  Connections-User-Controller",
             callback: function (done) {
 
                 chai.request(appUrl)
-                    .delete("/user/delete-connection/:id")
+                    .delete("user/delete-connection/:id")
                     .end(function (err, res) {
                         try {
-                            res.should.have.status(200);
+                            res.should.have.status(401);
                             res.body.should.have.property('error', true);
                             res.body.should.have.property('msg');
+                            done();
                         }
                          catch (error) {
                             done(error);
@@ -197,6 +164,5 @@ module.exports = function (appUrl, chai, should, assert, models) {
                     });
             }
         },
-        
     ];
 };
--- tests/testData/models/connections.js
+++ tests/testData/models/connections.js
@@ -1,28 +1,18 @@
 
-const { TEST_USER_MOBILE, TEST_USER_PASSWORD, TEST_USER_NEW_PASSWORD, TEST_USER_EMAIL, TEST_USER_NAME,TEST_USER_WRONG_MOBILE } = process.env
+const { TEST_USER_MOBILE, TEST_USER_PASSWORD, TEST_USER_NEW_PASSWORD, TEST_USER_EMAIL, TEST_USER_NAME,TEST_USER_WRONG_MOBILE, TEST_USER_CONNECTION1_NAME, TEST_USER_CONNECTION2_NAME,TEST_USER_CONNECTION1_MOBILE,TEST_USER_CONNECTION2_MOBILE } = process.env
 
-module.exports =
- {
-    appConnections1: {
-        data: {
-            'name' : TEST_USER_NAME,
-            'mobile': TEST_USER_MOBILE
+module.exports ={
+appConnections1: {
+    data: [
+        {
+            "name": TEST_USER_CONNECTION1_NAME,
+            "mobile": TEST_USER_CONNECTION1_MOBILE
+        },
+        {
+            "name": TEST_USER_CONNECTION2_NAME,
+            "mobile": TEST_USER_CONNECTION2_MOBILE
         }
-    },
-    appConnections2: {
-        data: {
-            'name' : TEST_USER_NAME
-        }
-    },
-    appConnections3: {
-        data: {
-            'mobile': TEST_USER_WRONG_MOBILE
-        }
-    },
-    appConnections4: {
-        data: {
-        }
-    },
- 
+    ]
 
+}
 };
