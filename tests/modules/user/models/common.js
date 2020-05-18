7210a3a427ef992ae5cd3f77888af2dbc9d813bc
Jay Gupta <jayguptazzz@gmail.com>
2020-05-18 14:21:24 +0530

    lookup api test cases

--- tests/modules/user/models/common.js
+++ tests/modules/user/models/common.js
@@ -5,11 +5,79 @@ module.exports = function (appUrl, chai, should, assert, models) {
             callback: function (done) {
 
                 chai.request(appUrl)
-                    .post('user/lookup')
+                    .get('common/lookup')
                     .end(function (err, res) {
                         try {
                             res.should.have.status(200);
-                            res.body.should.have.property('error', false, res.body.msg);
+                            res.body.should.have.property('error', false);
+                            done();
+                        } catch (error) {
+                            done(error);
+                        }
+                    });
+            }
+        },
+        {
+            description: "Get Lookup Case-2 Common-User-Controller",
+            callback: function (done) {
+
+                chai.request(appUrl)
+                    .get('common/lookup?requested_for=contact_details')
+                    .end(function (err, res) {
+                        try {
+                            res.should.have.status(200);
+                            res.body.should.have.property('error', false);
+                            done();
+                        } catch (error) {
+                            done(error);
+                        }
+                    });
+            }
+        },
+        {
+            description: "Get Lookup Case-3 Common-User-Controller",
+            callback: function (done) {
+
+                chai.request(appUrl)
+                    .get('common/lookup?requested_for=status_messages')
+                    .end(function (err, res) {
+                        try {
+                            res.should.have.status(200);
+                            res.body.should.have.property('error', false);
+                            done();
+                        } catch (error) {
+                            done(error);
+                        }
+                    });
+            }
+        },
+        {
+            description: "Get Lookup Case-4 Common-User-Controller",
+            callback: function (done) {
+
+                chai.request(appUrl)
+                    .get('common/lookup?requested_for=common_questions')
+                    .end(function (err, res) {
+                        try {
+                            res.should.have.status(200);
+                            res.body.should.have.property('error', false);
+                            done();
+                        } catch (error) {
+                            done(error);
+                        }
+                    });
+            }
+        },
+        {
+            description: "Get Lookup Case-5 Common-User-Controller",
+            callback: function (done) {
+
+                chai.request(appUrl)
+                    .get('common/lookup?requested_for=notification_messages')
+                    .end(function (err, res) {
+                        try {
+                            res.should.have.status(200);
+                            res.body.should.have.property('error', false);
                             done();
                         } catch (error) {
                             done(error);
