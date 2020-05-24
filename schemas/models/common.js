7d4f7ce3289a739b6c4d78b681964643f83a4b97
Jay Gupta <jayguptazzz@gmail.com>
2020-05-24 19:36:42 +0530

    getCountries API

---
+++ schemas/models/common.js
@@ -0,0 +1,11 @@
+module.exports = {
+    
+    getCountries: {
+        "properties": {
+            "requested_for": { "type": ["string"]},
+            "value": { "type": ["array"]},
+        },
+        "additionalProperties": false
+    },
+  
+}
