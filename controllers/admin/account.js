4cf3cb37ebff9eeea52583f9561d4b5a8d1fe9bd
Jay Gupta <jaygupta@Jays-MacBook-Air.local>
2020-07-16 20:46:58 +0530

    in progress

--- controllers/admin/account.js
+++ controllers/admin/account.js
@@ -1,76 +1,127 @@
-// let User = require("../../models/user");
+const resPerPage = process.env.RESPONSE_PER_PAGE || 10;
+const random = require("randomstring");
 
 module.exports.account = function (utils, collection) {
+
   const { User } = collection;
   return {
     getAllUsers: (request, response) => {
-      let limit = parseInt(request.query.limit) || 10;
-      let skip = parseInt(request.query.skip) || 0;
+      let page = parseInt(request.query.page) || 1;
+      let limit, skip, searchKeyword;
       let sort = {};
-      let searchKeyword = request.query.searchKeyword || "";
+
       if (request.query.sortBy && request.query.orderBy) {
-        sort[request.query.sortBy] = request.query.orderBy === "desc" ? -1 : 1;
+        sort[request.query.sortBy] = request.query.orderBy === 'desc' ? -1 : 1
       }
 
-      let field = request.query.field;
-      let value = request.query.value;
+      if (!(request.query.pagination && request.query.page)) {
+        limit = parseInt(request.query.limit) || resPerPage;
+        skip = parseInt(request.query.skip) || 0;
+        searchKeyword = request.query.searchKeyword || "";
+      } else {
+        limit = resPerPage;
+        skip = (page - 1) * resPerPage
+      }
 
-      User.getModel()
-        .find({ name: { $regex: new RegExp(searchKeyword) } })
+      let populate = {};
+      if (searchKeyword) {
+        populate["name"] = { "$regex": new RegExp(searchKeyword) }
+      }
+
+      let countQuery = User.count();
+
+      User.find(populate)
         .limit(limit)
         .skip(skip)
         .sort(sort)
-        .then((data) => {
-          let userData = {
-            userData: data,
-            totalResults: data.length,
-            limit,
-            // page: 1,
-            totalPages: 2,
-            hasPrevPage: skip > 10 ? true : false,
-            prevPage: skip > 10 ? parseInt(skip / 10) : 0,
-            // nextPage: 6,
-          };
-          utils.sendResponse(response, false, 200, 4038, userData);
-        })
-        .catch((error) => {
+        .then(data => {
+          countQuery.then((countData) => {
+            if (data && data.length) {
+              let result = {
+                "items": data,
+                "totalRecords": countData,
+                "totalResult": data.length,
+                "pagination": !(request.query.pagination && request.query.page) ? false : "",
+              }
+              if (request.query.pagination && request.query.page) {
+                result["pagination"] = {
+                  "totalRecords": countData,
+                  "totalPages": Math.ceil(countData / resPerPage),
+                  "currentPage": page,
+                  "resPerPage": resPerPage,
+                  "hasPrevPage": page > 1,
+                  "hasNextPage": page < Math.ceil(countData / resPerPage),
+                  "previousPage": page > 1 ? page - 1 : null,
+                  "nextPage": page < Math.ceil(countData / resPerPage) ? page + 1 : null
+                }
+              } else {
+                if (request.query.limit) {
+                  result["limit"] = limit
+                }
+                if (request.query.skip) {
+                  result["skip"] = skip
+                }
+              }
+              utils.sendResponse(response, false, 200, 4038, result);
+            } else {
+              utils.sendResponse(response, false, 200, 5000);
+            }
+          }).catch(error => {
+            console.log(error)
+            return error
+          })
+
+        }).catch((error) => {
           utils.sendResponse(response, true, 500, 1000);
-        });
+        })
     },
 
-    addUserByAdmin: (request, response) => {
-      let userInfo = request.body;
-      User.insertMany(userInfo)
-        .then((userData) => {
-          utils.sendResponse(response, false, 200, 4052, userData);
-        })
-        .catch((error) => {
-          utils.sendResponse(response, true, 500, 1000);
+    addUserByAdmin: async (request, response) => {
+      try {
+        let tempPassword = random.generate(6);
+        let userInfo = new User({
+          ...request.body,
+          password: tempPassword
         });
+        userInfo = await userInfo.save();
+        utils.sendResponse(response, false, 200, 4052, userInfo);
+      } catch (error) {
+        utils.sendResponse(response, true, 500, 1000);
+      }
     },
 
-    updateUserByAdmin: (request, response) => {
-      let userId = request.headers.payload.id;
-      const options = { new: true };
-      User.updateOne({ _id: userId }, { $set: request.body }, options)
-        .then((success) => {
-          utils.sendResponse(response, false, 200, 4053, userData);
-        })
-        .catch((error) => {
-          utils.sendResponse(response, true, 500, 1000);
-        });
+    updateUserByAdmin: async (request, response) => {
+      try {
+        let userId = request.params.userId;
+        let validate = utils.validateMongoId(userId);
+        if (!validate) {
+          return utils.sendResponse(response, true, 422, "MONGONODE422");
+        }
+        const options = { new: true };
+        let userDetail = await User.findOneAndUpdate({ _id: userId }, { $set: request.body }, options);
+        if (!userDetail) {
+          return utils.sendResponse(response, false, 422, 5000);
+        }
+        utils.sendResponse(response, false, 200, 4053, userDetail);
+      } catch (error) {
+        utils.sendResponse(response, true, 500, 1000);
+      }
     },
-
-    deleteUserByAdmin: (request, response) => {
-      let userId = request.headers.payload.id;
-      const options = { new: true };
-      User.deleteOne({ _id: userId })
-        .then((success) => {
-          utils.sendResponse(response, false, 200, 4054);
-        })
-        .catch((error) => {
-          utils.sendResponse(response, true, 500, 1000);
-        });
+    deleteUserByAdmin: async (request, response) => {
+      try {
+        let userId = request.params.userId;
+        let validate = utils.validateMongoId(userId);
+        if (!validate) {
+          return utils.sendResponse(response, true, 422, "MONGONODE422");
+        }
+        let userDetail = await User.findOneAndDelete({ _id: userId });
+        if (!userDetail) {
+          return utils.sendResponse(response, false, 422, 5000);
+        }
+        utils.sendResponse(response, false, 200, 4054, userDetail);
+      } catch (error) {
+        utils.sendResponse(response, true, 500, 1000);
+      }
     },
   };
 };
--- controllers/admin/setting.js
+++ controllers/admin/setting.js
@@ -1,46 +1,61 @@
 module.exports.setting = function (utils, collection) {
   const { Setting } = collection;
   return {
-    getSetting: (request, response) => {
-      let userId = request.headers.payload.id;
-      Setting.findById({ _id: userId })
-        .then((userData) => {
-          utils.sendResponse(response, false, 200, 4060, userData);
-        })
-        .catch((error) => {
-          utils.sendResponse(response, true, 500, 1000);
-        });
+    getSetting: async (request, response) => {
+      try {
+        let setting = await Setting.find({});
+        utils.sendResponse(response, false, 200, 4060, setting);
+      } catch (error) {
+        utils.sendResponse(response, true, 500, 1000);
+      }
     },
-    addSetting: (request, response) => {
-      let userId = request.headers.payload.id;
-      Setting.insertMany({ _id: userId })
-        .then((userData) => {
-          utils.sendResponse(response, false, 200, 4061, userData);
-        })
-        .catch((error) => {
-          utils.sendResponse(response, true, 500, 1000);
+    addSetting: async (request, response) => {
+      try {
+        let user_id = request.headers.payload.id;
+        let setting = new Setting({
+          user_id,
+          ...request.body
         });
+        setting = await setting.save();
+        utils.sendResponse(response, false, 200, 4061, setting);
+      } catch (error) {
+        utils.sendResponse(response, true, 500, 1000);
+      }
     },
-    updateSetting: (request, response) => {
-      let userId = request.headers.payload.id;
-      const options = { new: true };
-      Setting.updateOne({ _id: userId }, { $set: request.body }, options)
-        .then((success) => {
-          utils.sendResponse(response, false, 200, 4062);
-        })
-        .catch((error) => {
-          utils.sendResponse(response, true, 500, 1000);
-        });
+    updateSetting: async (request, response) => {
+      try {
+        let settingId = request.params.settingId;
+        let validate = utils.validateMongoId(settingId);
+        if (!validate) {
+          return utils.sendResponse(response, true, 422, "MONGONODE422");
+        }
+        const options = { new: true };
+        let setting = await Setting.findOneAndUpdate({ _id: settingId }, { $set: request.body }, options);
+        if (!setting) {
+          return utils.sendResponse(response, false, 422, 5000);
+        }
+        utils.sendResponse(response, false, 200, 4062, setting);
+      } catch (error) {
+        utils.sendResponse(response, true, 500, 1000);
+      }
+
     },
-    deteteSetting: (request, response) => {
-      let userId = request.headers.payload.id;
-      Setting.delete({ _id: userId })
-        .then((userData) => {
-          utils.sendResponse(response, false, 200, 4063, userData);
-        })
-        .catch((error) => {
-          utils.sendResponse(response, true, 500, 1000);
-        });
+    deteteSetting: async (request, response) => {
+      try {
+        let settingId = request.params.settingId;
+        let validate = utils.validateMongoId(settingId);
+        if (!validate) {
+          return utils.sendResponse(response, true, 422, "MONGONODE422");
+        }
+        let setting = await Setting.findOneAndDelete({ _id: settingId });
+        if (!setting) {
+          return utils.sendResponse(response, false, 422, 5000);
+        }
+        utils.sendResponse(response, false, 200, 4063, setting);
+      } catch (error) {
+        console.log(error)
+        utils.sendResponse(response, true, 500, 1000);
+      }
     },
   };
 };
