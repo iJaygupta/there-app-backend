02b32d94b429ac31b17f518c24927b4f2fe011b3
amitsarraf <amitsarraf80@gmail.com>
2020-07-19 01:08:57 -0700

    chatroom & messages

--- controllers/user/chat.js
+++ controllers/user/chat.js
@@ -1,5 +1,5 @@
 module.exports.chat = function (utils, collection) {
-    const { Chatroom } = collection;
+    const { Chatroom, Message } = collection;
 
     return {
         createChatroom: async (request, response) => {
@@ -37,7 +37,99 @@ module.exports.chat = function (utils, collection) {
                 utils.sendResponse(response, true, 500, 1000, err);
             }
 
-        }
+        },
+        updateChatroom: async(request,response) =>{
+            try {
+                let chatroomId = request.params.chatroomId;
+                let validate = utils.validateMongoId( chatroomId);
+                if (!validate) {
+                  return utils.sendResponse(response, true, 422, "MONGONODE422");
+                }
+                const options = { new: true };
+                let chatroom = await Chatroom.findOneAndUpdate({ _id:  chatroomId }, { $set: request.body }, options);
+                if (!chatroom) {
+                  return utils.sendResponse(response, false, 422, 5000);
+                }
+                utils.sendResponse(response, false, 200, 4066, chatroom);
+              } catch (error) {
+                utils.sendResponse(response, true, 500, 1000);
+              }
+        
+        },
+        deleteChatroom: async (request, response) => {
+            try {
+              let chatroomId = request.params.chatroomId;
+              let validate = utils.validateMongoId(chatroomId);
+              if (!validate) {
+                return utils.sendResponse(response, true, 422, "MONGONODE422");
+              }
+              let chatroom = await Chatroom.findOneAndDelete({ _id: chatroomId });
+              if (!chatroom) {
+                return utils.sendResponse(response, false, 422, 5000);
+              }
+              utils.sendResponse(response, false, 200, 4067, chatroom);
+            } catch (error) {
+              console.log(error)
+              utils.sendResponse(response, true, 500, 1000);
+            }
+          },
+          
+          getMessgae: async (request, response) => {
+            try {
+              let message = await Message.find({});
+              utils.sendResponse(response, false, 200, 4068, message);
+            } catch (error) {
+              utils.sendResponse(response, true, 500, 1000);
+            }
+          },
+          addMessage: async (request, response) => {
+            try {
+              let user_id = request.headers.payload.id;
+              let message = new Message({
+                user_id,
+                ...request.body
+              });
+              message = await message.save();
+              utils.sendResponse(response, false, 200, 4069, message);
+            } catch (error) {
+              utils.sendResponse(response, true, 500, 1000);
+            }
+          },
+          updateMessage: async (request, response) => {
+            try {
+              let messageId = request.params.messageId;
+              let validate = utils.validateMongoId(messageId);
+              if (!validate) {
+                return utils.sendResponse(response, true, 422, "MONGONODE422");
+              }
+              const options = { new: true };
+              let message = await Message.findOneAndUpdate({ _id: messageId}, { $set: request.body }, options);
+              if (!message) {
+                return utils.sendResponse(response, false, 422, 5000);
+              }
+              utils.sendResponse(response, false, 200, 4070, message);
+            } catch (error) {
+              utils.sendResponse(response, true, 500, 1000);
+            }
+      
+          },
+          deteteMessage: async (request, response) => {
+            try {
+              let messageId = request.params.messageId;
+              let validate = utils.validateMongoId(messageId);
+              if (!validate) {
+                return utils.sendResponse(response, true, 422, "MONGONODE422");
+              }
+              let message = await Message.findOneAndDelete({ _id: messageId });
+              if (!message) {
+                return utils.sendResponse(response, false, 422, 5000);
+              }
+              utils.sendResponse(response, false, 200, 4071, message);
+            } catch (error) {
+              console.log(error)
+              utils.sendResponse(response, true, 500, 1000);
+            }
+          },
 
     }
 }
--- routes/user/chat.js
+++ routes/user/chat.js
@@ -18,7 +18,7 @@ module.exports.chat = function (app, controller, error, auth, middleware, schema
 
   app.route("/user/chatroom/:chatroomId").put(auth, function (request, response) {
     try {
-      controller.createChatroom(request, response);
+      controller.updateChatroom(request, response);
     } catch (err) {
       error(err, response);
     }
@@ -26,9 +26,46 @@ module.exports.chat = function (app, controller, error, auth, middleware, schema
 
   app.route("/user/chatroom/:chatroomId").delete(auth, function (request, response) {
     try {
-      controller.createChatroom(request, response);
+      controller.deleteChatroom(request, response);
     } catch (err) {
       error(err, response);
     }
   });
+  app.route("/user/chatroom/:chatroomId").post(auth, function (request, response) {
+    try {
+      controller.joinChatroom(request, response);
+    } catch (err) {
+      error(err, response);
+    }
+  });
+  app.route("/user/message").get(auth, function (request, response) {
+    try {
+      controller.getMessgae(request, response);
+    } catch (err) {
+      error(err, response);
+    }
+  });
+  app.route("/user/message").post(auth, function (request, response) {
+    try {
+      controller.addMessage(request, response);
+    } catch (err) {
+      error(err, response);
+    }
+  });
+  app.route("/user/message/:messageId").put(auth, function (request, response) {
+    try {
+      controller.updateMessage(request, response);
+    } catch (err) {
+      error(err, response);
+    }
+  });
+  app.route("/user/message/:messageId").delete(auth, function (request, response) {
+    try {
+      controller.deteteMessage(request, response);
+    } catch (err) {
+      error(err, response);
+    }
+  });
+
+  
 }
