b3516e86002036f3c8fca59210ae3d815e6a7395
iharshk <harshkesharwani1616@gmai.com>
2020-08-01 01:14:55 -0700

    get connections & get blocked connections

--- controllers/user/connections.js
+++ controllers/user/connections.js
@@ -1,12 +1,150 @@
 
+const resPerPage = process.env.RESPONSE_PER_PAGE || 10;
 exports.connections = function (utils, collection) {
     const { Connections, User } = collection;
     return {
-        getConnections: async (request, response) => {
+        getConnections: async function (request, response) {
             try {
-                let user_id = request.headers.payload.id;
-                let connection = await Connections.find({ user_id: user_id }).populate("contact_list").populate("blocked_list").exec();
-                utils.sendResponse(response, false, 200, 4028, connection);
+                let user_id = request.headers && request.headers.payload && request.headers.payload.id;
+                let limit, skip, isActive, sortBy, orderBy, page;
+                let populateQuery = { path: "contact_list" };
+                if (request && request.query) {
+                    isActive = request.query.is_active || null;
+                    sortBy = request.query.sortBy || null;
+                    orderBy = request.query.orderBy || null; // 'desc' or 'asc'
+                    page = parseInt(request.query.page) || 1;
+                    searchKeyword = request.query.searchKeyword || null; // search by name
+                }
+                if (!(request.query && request.query.pagination && request.query.page)) {
+                    limit = parseInt(request.query.limit) || resPerPage;
+                    skip = parseInt(request.query.skip) || 0;
+                } else {
+                    limit = resPerPage;
+                    skip = (page - 1) * resPerPage
+                }
+                if (isActive) {
+                    isActive = JSON.parse(isActive);
+                    populateQuery['match'] = { 'is_active': isActive }
+                }
+                if (sortBy && (orderBy == 'desc' || orderBy == 'asc')) {
+                    let order = orderBy === 'desc' ? -1 : 1;
+                    populateQuery['options'] = {};
+                    populateQuery['options']['sort'] = {}
+                    populateQuery['options']['sort'][sortBy] = order;
+                }
+
+                await Connections.find({ user_id: user_id }).populate(populateQuery).exec((err, data) => {
+                    let res = [];
+                    data.map((elem, i) => {
+                        let result = {}
+                        result['totalRecords'] = elem.contact_list.length;
+                        result.contact_list = elem.contact_list.splice(skip, limit);
+                        result['totalResults'] = result.contact_list.length;
+                        if (searchKeyword) {
+                            let a = [];
+                            result.contact_list.map(el => {
+                                if (el['name'].indexOf(searchKeyword) != -1) a.push(el);
+                            });
+                            result.contact_list = a;
+                        }
+                        if (request.query.pagination && request.query.page) {
+                            result["pagination"] = {
+                                "totalRecords": result['totalRecords'],
+                                "totalPages": Math.ceil(result['totalRecords'] / resPerPage),
+                                "currentPage": page,
+                                "resPerPage": resPerPage,
+                                "hasPrevPage": page > 1,
+                                "hasNextPage": page < Math.ceil(result['totalRecords'] / resPerPage),
+                                "previousPage": page > 1 ? page - 1 : null,
+                                "nextPage": page < Math.ceil(result['totalRecords'] / resPerPage) ? page + 1 : null
+                            }
+                        } else {
+                            result["pagination"] = false;
+                            if (request.query.limit) {
+                                result["limit"] = limit
+                            }
+                            if (request.query.skip) {
+                                result["skip"] = skip
+                            }
+                        }
+                        res.push(result);
+                    });
+                    utils.sendResponse(response, false, 200, 4028, res);
+                });
+            }
+            catch (error) {
+                utils.sendResponse(response, true, 500, 1000);
+            }
+        },
+
+        getBlockedConnections: async function (request, response) {
+            try {
+                let user_id = request.headers && request.headers.payload && request.headers.payload.id;
+                let limit, skip, isActive, sortBy, orderBy, page;
+                let populateQuery = { path: "blocked_list" };
+                if (request && request.query) {
+                    isActive = request.query.is_active || null;
+                    sortBy = request.query.sortBy || null;
+                    orderBy = request.query.orderBy || null; // 'desc' or 'asc'
+                    page = parseInt(request.query.page) || 1;
+                    searchKeyword = request.query.searchKeyword || null; // search by name
+                }
+                if (!(request.query && request.query.pagination && request.query.page)) {
+                    limit = parseInt(request.query.limit) || resPerPage;
+                    skip = parseInt(request.query.skip) || 0;
+                } else {
+                    limit = resPerPage;
+                    skip = (page - 1) * resPerPage
+                }
+                if (isActive) {
+                    isActive = JSON.parse(isActive);
+                    populateQuery['match'] = { 'is_active': isActive }
+                }
+                if (sortBy && (orderBy == 'desc' || orderBy == 'asc')) {
+                    let order = orderBy === 'desc' ? -1 : 1;
+                    populateQuery['options'] = {};
+                    populateQuery['options']['sort'] = {}
+                    populateQuery['options']['sort'][sortBy] = order;
+                }
+
+                await Connections.find({ user_id: user_id }).populate(populateQuery).exec((err, data) => {
+                    let res = [];
+                    data.map((elem, i) => {
+                        let result = {}
+                        result['totalRecords'] = elem.blocked_list.length;
+                        result.blocked_list = elem.blocked_list.splice(skip, limit);
+                        result['totalResults'] = result.blocked_list.length;
+                        if (searchKeyword) {
+                            let a = [];
+                            result.blocked_list.map(el => {
+                                if (el['name'].indexOf(searchKeyword) != -1) a.push(el);
+                            });
+                            result.blocked_list = a;
+                        }
+                        if (request.query.pagination && request.query.page) {
+                            result["pagination"] = {
+                                "totalRecords": result['totalRecords'],
+                                "totalPages": Math.ceil(result['totalRecords'] / resPerPage),
+                                "currentPage": page,
+                                "resPerPage": resPerPage,
+                                "hasPrevPage": page > 1,
+                                "hasNextPage": page < Math.ceil(result['totalRecords'] / resPerPage),
+                                "previousPage": page > 1 ? page - 1 : null,
+                                "nextPage": page < Math.ceil(result['totalRecords'] / resPerPage) ? page + 1 : null
+                            }
+                        } else {
+                            result["pagination"] = false;
+                            if (request.query.limit) {
+                                result["limit"] = limit
+                            }
+                            if (request.query.skip) {
+                                result["skip"] = skip
+                            }
+                        }
+                        res.push(result);
+                    });
+                    utils.sendResponse(response, false, 200, 4028, res);
+                });
             }
             catch (error) {
                 utils.sendResponse(response, true, 500, 1000);
@@ -40,10 +178,11 @@ exports.connections = function (utils, collection) {
         prepareQueryForUpdateUserConnections: function (connections) {
             let bulkWriteConnectionQuery = [];
             bulkWriteConnectionQuery = connections.map((el, index) => {
+                el['is_active'] = false;
                 return {
                     updateOne: {
                         "filter": { mobile: el.mobile },
-                        "update": { $set: el },
+                        "update": { $setOnInsert: el },
                         "upsert": true
                     }
                 }
@@ -145,7 +284,7 @@ exports.connections = function (utils, collection) {
                             let res = {
                                 "blocked_connections": validConnections
                             }
-                            if (invalidConnections.length > 0) res["not_found"] = invalidConnections;
+                            if (invalidConnections.length) res["not_found"] = invalidConnections;
                             utils.sendResponse(response, false, 200, 4073, res);
                         } else utils.sendResponse(response, true, 500, 1000);
                     } else {
