8cb5f6619ebb09a0e46dad84231a747266f3911a
diksha-2 <diksha47149@gmail.com>
2020-08-13 11:47:00 +0530

    schedule testscript

--- tests/testData/models/schedule.js
+++ tests/testData/models/schedule.js
@@ -2,32 +2,34 @@ module.exports = {
 
     addSchedule1: {
         data: {
-            "meeting_time": { "start_time": "10am", "end_time" : "11am"},
-            "work_timing": {"start_time": "9am", "end_time" : "7pm" },
+            "meeting_time": { "start_time": "10am", "end_time": "11am" },
+            "work_timing": { "start_time": "9am", "end_time": "7pm" },
             "user_type": "Employed",
-            "sleeping_time": { "start_time": "10pm", "end_time" : "6am" },
-            "weekly_holiday":  ["Saturday", "Sunday"],
+            "sleeping_time": { "start_time": "10pm", "end_time": "6am" },
+            "weekly_holiday": ["Saturday", "Sunday"],
             "is_mood_off": true
         }
     },
 
     addSchedule2: {
         data: {
-            "sleeping_time": { "start_time": "10pm", "end_time" : "6am" }
+            "sleeping_time": { "start_time": "10pm", "end_time": "6am" }
         }
     },
     scheduleUpdate1: {
         data: {
-            "meeting_time": { "start_time": "10:30am", "end_time" : "11:30am"},
-            "work_timing": {"start_time": "9am", "end_time" : "7pm" },
+            "meeting_time": { "start_time": "10:30am", "end_time": "11:30am" },
+            "work_timing": { "start_time": "9am", "end_time": "7pm" },
             "user_type": "Employed",
-            "sleeping_time": { "start_time": "10pm", "end_time" : "6am" },
-            "weekly_holiday":  ["Saturday", "Sunday"],
+            "sleeping_time": { "start_time": "10pm", "end_time": "6am" },
+            "weekly_holiday": ["Saturday", "Sunday"],
             "is_mood_off": false
         }
     },
-     scheduleUpdate2: {
-        data: []
+    scheduleUpdate2: {
+        data: {
+            "user_type": "Employed"
+        }
     },
 
 
